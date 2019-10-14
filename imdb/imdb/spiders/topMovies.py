5cf70c124e259bbc4d7d25e1757376c2c37aa6dd
saeid doroudi <doroudi@live.com>
2019-10-14 23:47:00 +0330

    print movie names

--- imdb/imdb/spiders/topMovies.py
+++ imdb/imdb/spiders/topMovies.py
@@ -5,7 +5,10 @@ import scrapy
 class TopmoviesSpider(scrapy.Spider):
     name = 'topMovies'
     allowed_domains = ['https://www.imdb.com/chart/top']
-    start_urls = ['http://https://www.imdb.com/chart/top/']
+    start_urls = ['https://www.imdb.com/chart/top/']
 
     def parse(self, response):
+        titles = response.css(".lister-list .titleColumn a::text").extract()
+        for title in titles:
+            print(title)
         pass
