260b3f5f2216afd4ffaa27b08bf4e122b6bcc45d
saeid doroudi <doroudi@live.com>
2019-10-15 01:11:15 +0330

    get movies basic info

--- imdb/imdb/spiders/topMovies.py
+++ imdb/imdb/spiders/topMovies.py
@@ -8,7 +8,12 @@ class TopmoviesSpider(scrapy.Spider):
     start_urls = ['https://www.imdb.com/chart/top/']
 
     def parse(self, response):
-        titles = response.css(".lister-list .titleColumn a::text").extract()
-        for title in titles:
-            print(title)
-        pass
+        movies = response.css(".lister-list tr")
+        for movie in movies:
+            yield {
+                'image': movie.css('.posterColumn img::attr(src)').extract_first(),
+                'title': movie.css('.titleColumn a::text').extract_first(),
+                'link': movie.css('.titleColumn a::text').extract_first(),
+                'rate': movie.css('.ratingColumn strong::text').extract_first()
+            }
+            pass
