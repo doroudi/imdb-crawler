e16efb56c64bc010e2b49fd2fa3c7223aa5d97d7
saeid doroudi <doroudi@live.com>
2019-10-18 00:51:45 +0330

    get movies details

--- imdb/imdb/items.py
+++ imdb/imdb/items.py
@@ -8,7 +8,19 @@
 import scrapy
 
 
-class ImdbItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class MovieItem(scrapy.Item):
+    url = scrapy.Field()
+    name = scrapy.Field()
+    image = scrapy.Field()
+    genre = scrapy.Field()
+    contentRating = scrapy.Field()
+    actor = scrapy.Field()
+    creator = scrapy.Field()
+    description = scrapy.Field()
+    datePublished = scrapy.Field()
+    keywords = scrapy.Field()
+    aggregateRating = scrapy.Field()
+    review = scrapy.Field()
+    trailer = scrapy.Field()
+    duration = scrapy.Field()
     pass
--- imdb/imdb/spiders/topMovies.py
+++ imdb/imdb/spiders/topMovies.py
@@ -1,19 +1,48 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
+from ..items import MovieItem
 
 
 class TopmoviesSpider(scrapy.Spider):
     name = 'topMovies'
-    allowed_domains = ['https://www.imdb.com/chart/top']
+    allowed_domains = ['imdb.com']
     start_urls = ['https://www.imdb.com/chart/top/']
 
     def parse(self, response):
-        movies = response.css(".lister-list tr")
-        for movie in movies:
-            yield {
-                'image': movie.css('.posterColumn img::attr(src)').extract_first(),
-                'title': movie.css('.titleColumn a::text').extract_first(),
-                'link': movie.css('.titleColumn a::text').extract_first(),
-                'rate': movie.css('.ratingColumn strong::text').extract_first()
-            }
-            pass
+        links = response.css(".lister-list tr a::attr(href)").extract()
+        for link in links:
+            link = response.urljoin(link)
+            print(link)
+            yield scrapy.Request(url=link, callback=self.parse_details)
+
+    def parse_details(self, response):
+        json_res = json.loads(response.xpath(
+            "//script[@type='application/ld+json']/text()").extract_first())
+        film = MovieItem()
+        if 'name' in json_res:
+            film['name'] = json_res['name']
+        if 'image' in json_res:
+            film['image'] = json_res['image']
+        if 'genre' in json_res:
+            film['genre'] = json_res['genre']
+        if 'contentRating' in json_res:
+            film['contentRating'] = json_res['contentRating']
+        if 'actor' in json_res:
+            film['actor'] = json_res['actor']
+        if 'creator' in json_res:
+            film['creator'] = json_res['creator']
+        if 'description' in json_res:
+            film['description'] = json_res['description']
+        if 'keywords' in json_res:
+            film['keywords'] = json_res['keywords']
+        if 'aggregateRating' in json_res:
+            film['aggregateRating'] = json_res['aggregateRating']
+        if 'review' in json_res:
+            film['review'] = json_res['review']
+        if 'trailer' in json_res:
+            film['trailer'] = json_res['trailer']
+        if 'duration' in json_res:
+            film['duration'] = json_res['duration']
+
+        yield film
